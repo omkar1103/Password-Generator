b82d9d0a52ad66c9ebc4469396e0fdb6dfd8c695
omkar1103 <115880514+omkar1103@users.noreply.github.com>
2025-03-02 18:54:00 +0530

    Made changes in viteconfig.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ReactJs-Projects/05PasswordGenerator/',  // Replace with your repo name
-  build: {
-    outDir: 'dist',  // This is the default output directory for Vite
-  },
-})
+  base: './', // Add this line for proper asset resolution
+});
